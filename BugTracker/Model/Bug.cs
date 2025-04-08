e9f215f8588e11b178fae3a8857b603eae0647c9
rinat.abzalov <rinat.abzalov@spglobal.com>
2025-04-08 22:04:31 +0300

    Добавление комментариев для модели бага

--- BugTracker/Model/Bug.cs
+++ BugTracker/Model/Bug.cs
@@ -2,10 +2,23 @@
 {
     public class Bug
     {
+        /// <summary>
+        /// Идентификатор бага
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Наименование
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
         public string? Description { get; set; }
+
+        /// <summary>
+        /// Автор
+        /// </summary>
         public string? Author { get; set; }
-                
+
     }
 }
